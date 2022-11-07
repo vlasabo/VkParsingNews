2438eb36246ccd5c9eefa6fd211e9b72f5344afc
vlasabo <vladimirsabo@yandex.ru>
2022-11-07 10:07:26 +0300

    init commit

---
+++ src/main/java/com/bot/VkParsingBot/VkParsingBotApplication.java
@@ -0,0 +1,13 @@
+package com.bot.VkParsingBot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VkParsingBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VkParsingBotApplication.class, args);
+	}
+
+}
